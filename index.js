e71ee918c07caf5c7672dc496e3e8442be8e6228
Pablo.Carrero1 <pablo.carrero@ibm.com>
2023-12-29 00:46:45 +0100

    s

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ async function askQuestions() {
   try {
     const gameName = await getCurrentStreamGame();
     const aleatoryIndex = Math.floor(Math.random() * 50);
-    const question = questions[gameName][aleatoryIndex];
+    const questionF = questions[gameName][aleatoryIndex];
 
-    sendQuestionToChat(question);
+    sendQuestionToChat(questionF);
   } catch (error) {
     sendQuestionToChat('No se ha podido obtener una pregunta...');
   }
